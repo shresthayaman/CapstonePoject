2783e3477b5237eb5b9e11a0096f4472483ad919
Yaman Shrestha <shresthayaman@hotmail.com>
2020-11-02 18:25:01 -0500

    Modified main for starting

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
+
+
+
+/************************************************************************************************************************************
+ * Delong's Fan Club
+ *
+ * Date: November 2020
+ * Author(s): Yaman Shrestha, Sunny Hwang, Jack Galleta
+ *
+ */
+
 #include "msp.h"
 
-
-/**
- * main.c
- */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	//Initialize variables
+	//Initialize hardware
+	//Finally enable interrupts
+
+	while(1){
+
+	}
+
+	return 0;
 }
